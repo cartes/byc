7034bc12ca38fd20636572b47ab4eb03172a9b75
cartes <cristiabncartesa@gmail.com>
2020-01-29 17:51:17 -0300

    Ide-helper Models

--- app/Commune.php
+++ app/Commune.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Commune
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $region_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune whereRegionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Commune whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Commune extends Model
 {
     //
--- app/UserMeta.php
+++ app/UserMeta.php
@@ -4,6 +4,38 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\UserMeta
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $rut
+ * @property string|null $gender
+ * @property string|null $address
+ * @property int|null $commune_id
+ * @property int|null $region_id
+ * @property string|null $city
+ * @property string|null $province
+ * @property string|null $birthday
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereBirthday($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereCommuneId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereGender($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereProvince($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereRegionId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereRut($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\UserMeta whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UserMeta extends Model
 {
     //

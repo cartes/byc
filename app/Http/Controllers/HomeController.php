c163e270e2cf3f7de66b90c1c9d2712c6b9a9a16
cartes <cristiabncartesa@gmail.com>
2020-01-30 17:42:01 -0300

    Query para mostrar las ofertas en el home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +14,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::with('category', 'seller')
+            ->where('status', Post::PUBLISHED)
+            ->latest()
+            ->paginate(20);
+        return view('home', compact('posts'));
     }
 }
